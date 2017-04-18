7d5366d3bace049b5889cdff87084b5abbec4775
Daffyta <daffy.dine9@gmail.com>
2017-04-18 11:06:19 -0500

    Restaurando Imagenes

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ function ocultarImagen() {
   this.style.display = "none";
 }
 
-// var imagenesTotales= document.getElementsByClassName("casitaImagen");
-// var tachecito=document.getElementsByClassName("tacheCerrar");
-//
-// for(var i=0;i<tachecito.length;i++){
-//   imagenesTotales[i].addEventListener("click",ocultarImagen);
-// }
-// function ocultarImagen() {
-//   this.style.display="none";
+var botonRestaurar = document.getElementById('restaurando');
+botonRestaurar.addEventListener("click",restaurar);
+
+function restaurar(){
+  for(var i=0; i < eliminarElem.length;i++){
+      imgTotales[i].style.display="inline-block";
+    }
+}
